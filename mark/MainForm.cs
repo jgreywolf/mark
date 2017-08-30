fee9132a300915094d0ef274fad6364fa60317f6
intfrog <intfrog@gmail.com>
2017-08-30 14:26:27 +0800

    support console parameters

--- mark/MainForm.cs
+++ mark/MainForm.cs
@@ -5,16 +5,56 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Windows.Forms;
 using mark.Processor;
+using mark.Utils;
 
 namespace mark
 {
     public partial class MainForm : Form
     {
-        public MainForm()
+        private readonly string currentDir = Environment.CurrentDirectory;
+        private string currentFilePath;
+        private StreamWriter currentFileWriter;
+
+        public MainForm(string[] args)
         {
             InitializeComponent();
+            if (args.Length == 0)
+                init();
+            else
+                init(args[0]);
+        }
+
+        private void init(string filePath)
+        {
+            if (!Path.IsPathRooted(filePath))
+                filePath = Path.GetFullPath(Path.Combine(currentDir, filePath));
+            try
+            {
+                currentFileWriter = FileUtils.getStreamWriter(filePath);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Failed to open the file");
+                this.init();
+                return;
+            }
+            if (currentFileWriter == null)
+            {
+                this.init();
+                return;
+            }
+            this.currentFilePath = filePath;
+            this.Text = "MarkEditor - " + this.currentFilePath;
+        }
+
+        private void init()
+        {
+            this.currentFilePath = null;
+            this.currentFileWriter = null;
+            this.Text = "MarkEditor - " + "Untitled";
         }
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
---
+++ mark/Utils/FileUtils.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace mark.Utils
+{
+    class FileUtils
+    {
+        public static StreamWriter getStreamWriter(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite);
+                return new StreamWriter(fs);
+            }
+            else
+            {
+                DialogResult dialogResult = MessageBox.Show(
+                    "File does not exist. Would you like to create this file?",
+                    "MarkEditor", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                    return new StreamWriter(fs);
+                }
+            }
+            return null;
+        }
+    }
+}
